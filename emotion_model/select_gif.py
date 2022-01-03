70feab0866e2064757e02cca893b82432d9670c7
renaju <yijen.rena.ju@gmail.com>
2022-01-03 16:10:04 +0800

    new select

--- emotion_model/select_gif.py
+++ emotion_model/select_gif.py
@@ -1,5 +1,3 @@
-# from predict import predict
-
 emotions = {
     'Happy':7,
     'Surprised':6,
@@ -11,15 +9,16 @@ emotions = {
     'Fearful':0,
 }
 
-def select_gif(curr_mood, curr_gif):
-    # dictionary stores index of each gif with its corresponding weight
-    gif_indices = range(40)
-    weights = { i : 1/40 for i in gif_indices }
+prev_mood = 'Neutral'
+curr_gif = 0
+# dictionary stores index of each gif with its corresponding weight
+gif_indices = range(40)
+weights = { i : 1/40 for i in gif_indices }
 
+def select_gif(prev_mood, curr_mood, curr_gif):
     # update weights: 1.5 * 情緒晉級程度
     """
-    weights[curr_gif] *= (emotions[predict(config, audio_path, model)] - emotions[curr_mood]) * 1.5
+    weights[curr_gif] *= (emotions[curr_mood] - emotions[prev_mood]) * 1.5
     """
-
     # return max
     return max(weights, key=weights.get)
